bfebda8911e5d0f881dbc9758efe65948017c489
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-31 16:59:33 +0200

    simple array of strings in table view controller, without swift class

---
+++ Contacts/ContactsViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  Contacts
+//
+//  Created by Tomek Buslowski on 31.08.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+#import "ContactsViewController.h"
+
+@interface ContactsViewController ()
+
+@property (nonatomic, readonly, strong) NSMutableArray *contacts;
+
+@end
+
+@implementation ContactsViewController
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        NSArray *contactArray = @[@"Jan Kowalski",
+                                  @"Paweł Skaslki"];
+        
+        _contacts = [NSMutableArray arrayWithArray:contactArray];
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier: @"UITableViewCell"];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.contacts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    NSString *contact = self.contacts[indexPath.row];
+    
+    cell.textLabel.text = contact;
+    
+    return cell;
+}
+
+
+@end
