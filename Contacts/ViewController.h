02e9eb9225c46401def49c17e37924e7cce16f55
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-31 16:47:52 +0200

    initial commit

---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by Tomek Buslowski on 31.08.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by Tomek Buslowski on 31.08.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
