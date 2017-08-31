1d554454a18f46941fe15a06e64327e40d2ba431
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-31 17:03:40 +0200

    swift class added and bridging works

---
+++ Contacts/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Tomek Buslowski on 31.08.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    
+    let name: String
+    
+    init(contactName: String) {
+        self.name = contactName
+    }
+}
--- Contacts/ContactsViewController.m
+++ Contacts/ContactsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ContactsViewController.h"
+#import "Contacts-Swift.h"
 
 @interface ContactsViewController ()
 
